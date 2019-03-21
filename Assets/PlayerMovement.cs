a58c8a98ef1dd5a36c843fed444a963a64f3e0d8
Nolan Roher <maniacalspy@gmail.com>
2019-03-20 19:48:00 -0500

    Created Repository

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    float BaseMoveSpeed, MoveSpeed;
+    float SoundRadius;
+    CharacterController thisController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.gray;
+        SoundRadius = 0;
+        BaseMoveSpeed = 10f;
+        thisController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float XMoveSpeed = BaseMoveSpeed * Input.GetAxis("Horizontal");
+        float YMoveSpeed = BaseMoveSpeed * Input.GetAxis("Vertical");
+        MoveSpeed = BaseMoveSpeed;
+        if (Input.GetAxis("Walk") != 0)
+        {
+            XMoveSpeed /= 2;
+            YMoveSpeed /= 2;
+        }
+        else if (Input.GetAxis("Sprint") != 0)
+        {
+
+            XMoveSpeed *= 2;
+            YMoveSpeed *= 2;
+        }
+        Vector3 MoveVector = new Vector3(XMoveSpeed, 0, YMoveSpeed);
+        MoveSpeed = MoveVector.magnitude;
+        thisController.SimpleMove(MoveVector);
+        if (MoveSpeed > BaseMoveSpeed/2)
+        {
+            SoundRadius = MoveSpeed/2;
+        }
+        else SoundRadius = 0;
+        SendSound();
+
+    }
+
+    void SendSound()
+    {
+        Collider[] EnemiesToNotify = Physics.OverlapSphere(transform.position, SoundRadius, 1 << LayerMask.NameToLayer("Enemies"));
+        Debug.Log(EnemiesToNotify.Length);
+        foreach(Collider enemy in EnemiesToNotify)
+        {
+            SoundEnemy SEScript = enemy.GetComponent<SoundEnemy>();
+            SEScript.PlayerFoundAction.Invoke();
+        }
+    }
+}
---
+++ Assets/SoundEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SoundEnemy : MonoBehaviour
+{
+    public UnityAction PlayerFoundAction;
+    float MoveSpeed;
+    GameObject Player;
+    Vector3 SearchPoint;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SearchPoint = new Vector3(0, 0, 0);
+        Player = GameObject.Find("Player");
+        MoveSpeed = 5f;
+        PlayerFoundAction += OnPlayerFound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SearchPoint.magnitude > 0)
+        {
+            rb.AddForce(SearchPoint - transform.position);
+            if (Mathf.Abs((SearchPoint - transform.position).magnitude) < .5) SearchPoint = new Vector3(0, 0, 0);
+            //rb.MovePosition((SearchPoint));
+        }
+    }
+
+    void OnPlayerFound()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        SearchPoint = Player.transform.position;
+    }
+
+    void OnPlayerLost()
+    {
+
+    }
+}
